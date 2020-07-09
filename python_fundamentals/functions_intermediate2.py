e55872a66b052a510aacd4f48967f36172275e6a
JMoore <jmoore.codes@gmail.com>
2020-07-09 18:45:44 -0500

    Make alterations to final problem in functions_intermediate2.py

--- python_fundamentals/functions_intermediate2.py
+++ python_fundamentals/functions_intermediate2.py
@@ -55,21 +55,21 @@
 # Iterate Through a Dictionary with List Values
 # Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list.
 
-dojo = {
-   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-}
+# dojo = {
+#    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+#    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# }
 
-def printInfo(someDict):
-    summary = ''
-    for li in someDict:
-        summary += str(len(someDict[li])) + ' '
-        summary += f"{li.upper()}\n"
+# def printInfo(someDict):
+#     summary = ''
+#     for li in someDict:
+#         summary += str(len(someDict[li])) + ' '
+#         summary += f"{li.upper()}\n"
 
-        for place in someDict[li]:
-            summary += place + '\n'
+#         for place in someDict[li]:
+#             summary += place + '\n'
             
-        summary += '\n'
-    print(summary)
+#         summary += '\n'
+#     print(summary)
 
-printInfo(dojo)
+# printInfo(dojo)
