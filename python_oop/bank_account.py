c3b1c43f2a7e4723554239e4af74eaa8752a6187
JMoore <jmoore.codes@gmail.com>
2020-07-14 21:49:19 -0500

    Modularize users.py and bank_account.py and add multi-account functionality to users.py

--- python_oop/bank_account.py
+++ python_oop/bank_account.py
@@ -25,10 +25,3 @@ class BankAccount:
             print(f"Your account is overdrawn and cannot accrue interest.")
         return self
 
-Jonathan = BankAccount(400)
-
-Jonathan.deposit(250).deposit(250).deposit(250).withdraw(150).yield_interest().display_account_info()
-
-Andrew = BankAccount(0,0.05)
-
-Andrew.deposit(250).deposit(250).withdraw(150).withdraw(150).withdraw(150).withdraw(150).yield_interest().display_account_info()
--- python_oop/user.py
+++ python_oop/user.py
@@ -1,31 +1,38 @@
+from bank_account import BankAccount
+
 class User:
     def __init__(self, username, email, cash=0):
         self.name = username
         self.email = email
-        self.account_balance = cash
-    def make_deposit(self, amt):
-        self.account_balance += amt
+        self.accounts = {
+            'init_acct': BankAccount(cash, 0.02)
+        }
+    def make_deposit(self, amt, acct):
+        self.accounts[acct].balance += amt
         return self
-    def make_withdrawal(self, amt):
-        self.account_balance -= amt
+    def make_withdrawal(self, amt, acct):
+        self.accounts[acct].balance -= amt
         return self
-    def display_user_balance(self):
-        print(f"{self.name}, your current balance is ${self.account_balance}")
+    def display_user_balance(self, acct='All'):
+        if acct == 'All':
+            for a in self.accounts:
+                print(f"{self.name}, the current balance in your {a} account is ${self.accounts[a].balance}")
+        else:
+            print(f"{self.name}, the current balance in your {acct} account is ${self.accounts[acct].balance}")
         return self
-    def transfer_money(self, other_acct, amt):
-        self.account_balance -= amt
-        other_acct.account_balance += amt
-        print(f"{self.name}, your transfer of ${amt} to {other_acct.name} has been completed. Your new account balance is ${self.account_balance}.\nBecause we don't believe in privacy, here's {other_acct.name}'s new account balance: ${other_acct.account_balance}.")
+    def transfer_money(self, acct, rec_user, other_acct, amt):
+        self.accounts[acct].balance -= amt
+        rec_user.accounts[other_acct].balance += amt
+        print(f"{self.name}, your transfer of ${amt} from your {acct} account to {rec_user.name}'s {other_acct} has been completed.")
+        self.display_user_balance(acct)
+        return self
+    def add_account(self, name, cash=0, int_rate=0.01):
+        self.accounts[name] = (BankAccount(cash, int_rate))
         return self
 
-dan = User("Dan", "dan@rocketmail.com")
-bob = User("Bob", "bob@gmail.com", 400)
-karen = User("Karen", "karen@hotmail.com", 200)
+jonathan = User("Jonathan", "jonathan@email.com", 400)
+joshua = User("Joshua", "joshua@email.com", 400)
 
-dan.make_deposit(352).make_deposit(490).make_deposit(16).make_withdrawal(375).display_user_balance()
+jonathan.make_deposit(200,'init_acct').display_user_balance('init_acct').transfer_money('init_acct', joshua, 'init_acct', 100)
 
-bob.make_deposit(1600).make_deposit(360).make_withdrawal(40).make_withdrawal(26).display_user_balance()
-
-karen.make_deposit(1000).make_withdrawal(360).make_withdrawal(400).make_withdrawal(250).display_user_balance()
-
-dan.transfer_money(karen, 10)
+joshua.add_account('Savings').transfer_money('init_acct', joshua, 'Savings', 100).display_user_balance()
